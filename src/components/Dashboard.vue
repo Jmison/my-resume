8f7e37e0a27f290f2cd0f605c3a7fdd13ac7584c
Jamo <jamo@Jamos-MBP.fios-router.home>
2019-11-21 21:34:47 -0500

    Implemented a side bar that displays desired tabs and created a dark-theme

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app id="inspire">
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-teach</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>More About Me</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-desktop-mac-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Check Out My Projects</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-gesture-double-tap</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Get In Touch</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+
+      </v-list>
+
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Jamison Nixon  </v-toolbar-title>
+    </v-app-bar>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
+</script>
